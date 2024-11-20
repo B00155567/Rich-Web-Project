79359f1ef1608a2add42c847b05867cdbf8dbe70
B00155567 <146002025+B00155567@users.noreply.github.com>
2024-11-20 19:20:07 +0000

    With console update

--- src/app/api/getProducts/route.js
+++ src/app/api/getProducts/route.js
@@ -4,6 +4,7 @@ export async function GET(req, res) {
     console.log("in the api page")
     // =================================================
     const { MongoClient } = require('mongodb');
+    console.log("DB_ADDRESS:", process.env.DB_ADDRESS); // Log the environment variable
     const url = process.env.DB_ADDRESS;
     const client = new MongoClient(url);
     const dbName = 'app'; // database name

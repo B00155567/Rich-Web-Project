69f84c216a8e546a7ca621ed06b4e3386853fbb7
B00155567 <146002025+B00155567@users.noreply.github.com>
2024-11-30 21:11:52 +0000

    Update route.js

--- src/app/api/getProducts/route.js
+++ src/app/api/getProducts/route.js
@@ -1,8 +1,6 @@
 export async function GET(req, res) {
-    // Make a note we are on
-    // the api. This goes to the console.
+    // Make a note we are on the api. This goes to the console.
     console.log("in the api page")
-    // =================================================
     const { MongoClient } = require('mongodb');
     console.log("DB_ADDRESS:", process.env.DB_ADDRESS); // Log the environment variable
     const url = process.env.DB_ADDRESS;
@@ -14,8 +12,6 @@ export async function GET(req, res) {
     const collection = db.collection('products'); // collection name
     const findResult = await collection.find({}).toArray();
     console.log('Found documents =>', findResult);
-    //==========================================================
-    // at the end of the process we need to send something back.
     return Response.json(findResult)
     }
     
